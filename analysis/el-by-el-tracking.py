ccf77b12c98e1f1aa4a873226c2f79c0c5fbce18
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2020-02-11 16:47:44 +0300

    tl-by-el-tracking updated

--- analysis/el-by-el-tracking.py
+++ analysis/el-by-el-tracking.py
@@ -6,6 +6,15 @@ DATADIR = 'data/EL-BY-EL-TRACKING/'
 
 ps = load_ps(HOMEDIR+DATADIR)
 
-fig, ax = plt.subplots(2,1,sharex=True)
-ax[0].plot(ps['X'][:471]); ax[0].set_ylabel('X')
-ax[1].plot(ps['Y'][:471]); ax[1].set_ylabel('Y')
+
+def plot(turn_num=1):
+    fig, ax = plt.subplots(4,1,sharex=True)
+    ax[0].plot(ps['X'][:471*turn_num]); ax[0].set_ylabel('X')
+    ax[1].plot(ps['Y'][:471*turn_num]); ax[1].set_ylabel('Y')
+    ax[2].plot(ps['T'][:471*turn_num]); ax[2].set_ylabel('T')
+    ax[3].plot(ps['D'][:471*turn_num]); ax[3].set_ylabel('D')
+
+
+
+if __name__ == '__main__':
+    plot(3)
