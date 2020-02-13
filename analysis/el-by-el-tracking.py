4d9093ea92d7c783e805ec7d6e58a7415573f1ac
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2020-02-13 15:00:49 +0300

    working on producing the spin-on-the-CO rotatioon map (element by element)

--- analysis/el-by-el-tracking.py
+++ analysis/el-by-el-tracking.py
@@ -1,11 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
-from analysis import HOMEDIR, load_ps
+from importlib import reload
+import analysis as ana; reload(ana)
+
+HOMEDIR, load_ps, load_sp, _read_header = ana.HOMEDIR, ana.load_ps, ana.load_sp, ana._read_header
 
 DATADIR = 'data/EL-BY-EL-TRACKING/'
 
 ps = load_ps(HOMEDIR+DATADIR)
+sp = load_sp(HOMEDIR+DATADIR)
+ELNAMES = np.load(HOMEDIR+'analysis/nica_element_names.npy')
 
+def plot_spin(rng):
+    fig, ax = plt.subplots(3,1,sharex=True)
+    ax[0].plot(sp['S_X'][rng]); ax[0].set_ylabel('S_X')
+    ax[1].plot(sp['S_Y'][rng]); ax[1].set_ylabel('S_Y')
+    ax[2].plot(sp['S_Z'][rng]); ax[2].set_ylabel('S_Z')
+    ax[2].set_xticks(np.arange(len(ELNAMES)+1))
+    ax[2].set_xticklabels(ELNAMES, rotation=45)
 
 def plot(turn_num=1):
     fig, ax = plt.subplots(4,1,sharex=True)
@@ -17,4 +29,4 @@ def plot(turn_num=1):
 
 
 if __name__ == '__main__':
-    plot(3)
+    plot_spin(slice(0,471))
