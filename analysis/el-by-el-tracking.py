59c285afe1267c65686c9c61b4e69cc40a451aff
Alex Aksentyev <alexaksentyev@Alexs-MBP.Dlink>
2020-02-14 14:18:26 +0300

    el-by-el-tracking script: plotting spin tracking vs element names

--- analysis/el-by-el-tracking.py
+++ analysis/el-by-el-tracking.py
@@ -9,24 +9,27 @@ DATADIR = 'data/EL-BY-EL-TRACKING/'
 
 ps = load_ps(HOMEDIR+DATADIR)
 sp = load_sp(HOMEDIR+DATADIR)
-ELNAMES = np.load(HOMEDIR+'analysis/nica_element_names.npy')
 
-def plot_spin(rng):
+
+ELNAMES = np.insert(np.load('nica_element_names.npy'),0,'INJ')
+ELNAMES = np.array([e+' ['+ str(i) + ']' for i,e in enumerate(ELNAMES)])
+
+def plot_spin(dat):
     fig, ax = plt.subplots(3,1,sharex=True)
-    ax[0].plot(sp['S_X'][rng]); ax[0].set_ylabel('S_X')
-    ax[1].plot(sp['S_Y'][rng]); ax[1].set_ylabel('S_Y')
-    ax[2].plot(sp['S_Z'][rng]); ax[2].set_ylabel('S_Z')
-    ax[2].set_xticks(np.arange(len(ELNAMES)+1))
-    ax[2].set_xticklabels(ELNAMES, rotation=45)
+    ax[0].plot(dat['EID'], dat['S_X']); ax[0].set_ylabel('S_X')
+    ax[1].plot(dat['EID'], dat['S_Y']); ax[1].set_ylabel('S_Y')
+    ax[2].plot(dat['EID'], dat['S_Z']); ax[2].set_ylabel('S_Z')
+    ax[2].set_xlabel('EID')
+    plt.xticks(ticks=dat['EID'][:,0], labels=ELNAMES[dat['EID'][:,0]], rotation=60)
 
-def plot(turn_num=1):
+def plot(dat):
     fig, ax = plt.subplots(4,1,sharex=True)
-    ax[0].plot(ps['X'][:471*turn_num]); ax[0].set_ylabel('X')
-    ax[1].plot(ps['Y'][:471*turn_num]); ax[1].set_ylabel('Y')
-    ax[2].plot(ps['T'][:471*turn_num]); ax[2].set_ylabel('T')
-    ax[3].plot(ps['D'][:471*turn_num]); ax[3].set_ylabel('D')
+    ax[0].plot(dat['X']); ax[0].set_ylabel('X')
+    ax[1].plot(dat['Y']); ax[1].set_ylabel('Y')
+    ax[2].plot(dat['T']); ax[2].set_ylabel('T')
+    ax[3].plot(dat['D']); ax[3].set_ylabel('D')
 
 
 
 if __name__ == '__main__':
-    plot_spin(slice(0,471))
+    plot_spin(sp[:,[0, 1, 28]])
