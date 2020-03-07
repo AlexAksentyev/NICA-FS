48682a75689403f801f66304d7724a6604761a35
Alex Aksentyev <alexaksentyev@gmail.com>
2020-03-07 13:41:54 +0300

    reorganization of the mapview.py script

--- madx-scripts/mapview.py
+++ madx-scripts/mapview.py
@@ -76,47 +76,72 @@ def load_cosy(name, dir_=DIR):
     trans_map[:5,:] = tmp.T; trans_map[5,-1] = 1
     return trans_map
 
+def solmap(L, KS):
+    c = np.cos(KS*L)
+    s = np.sin(KS*L)
+    M = np.eye(4)
+    M[0,0] = c*c
+    M[0,1] = s*c/KS/2
+    M[0,2] = s*c
+    M[0,3] = s*s/KS/s
+    M[1,0] = -KS/2*c
+    M[1,1] = c*c
+    M[1,2] = -KS/2*s*s
+    M[1,3] = s*c
+    M[2,0] = -s*c
+    M[2,1] = -s*s/KS/2
+    M[2,2] = c*c
+    M[2,3] = s*c/KS/2
+    M[3,0] = KS/2*s*s
+    M[3,1] = -s*c
+    M[3,2] = -KS/2*s*c
+    M[3,3] = c*c
+    return M
+if __name__ == '__main__':
+    
 ## getting the MAD-X transfer map definition
-transfer_MADX = load_madx(mad_name)
+    transfer_MADX = load_madx(mad_name)
 
 ## same in COSY Infinity
-transfer_COSY = load_cosy(cosy_name)
+    transfer_COSY = load_cosy(cosy_name)
 
 
 ## printing
-print('********************')
-print('NAME:', cosy_name, '(COSY)/', mad_name, '(MADX)')
-print('********************')
-print('COSY[:2, :2]')
-print(transfer_COSY[:2, :2])
-print('MADX[:2, :2]')
-print(transfer_MADX[:2, :2])
-print('++ Difference')
-print(transfer_COSY[:2,:2]-transfer_MADX[:2,:2])
-print('COSY[2:4, 2:4]')
-print(transfer_COSY[2:4, 2:4])
-print('MADX[2:4, 2:4]')
-print(transfer_MADX[2:4, 2:4])
-print('++ Difference')
-print(transfer_COSY[2:4,2:4]-transfer_MADX[2:4,2:4])
+    print('********************')
+    print('NAME:', cosy_name, '(COSY)/', mad_name, '(MADX)')
+    print('********************')
+    print('COSY[:2, :2]')
+    print(transfer_COSY[:2, :2])
+    print('MADX[:2, :2]')
+    print(transfer_MADX[:2, :2])
+    print('++ Difference')
+    print(transfer_COSY[:2,:2]-transfer_MADX[:2,:2])
+    print('COSY[2:4, 2:4]')
+    print(transfer_COSY[2:4, 2:4])
+    print('MADX[2:4, 2:4]')
+    print(transfer_MADX[2:4, 2:4])
+    print('++ Difference')
+    print(transfer_COSY[2:4,2:4]-transfer_MADX[2:4,2:4])
 
-eldict = {'BH':'50','BV1':'10','BV2':'12','MQD1':'22','MQF1':'20','QD1':'2','QD2':'6','QF2':'4','SNK1':'26'}
-maparr = np.empty(len(eldict), dtype=object)
-for i, names in enumerate(eldict.items()):
-    maparr[i] = (load_madx(names[0]), load_cosy(names[1]))
+    eldict = {'BH':'50','BV1':'10','BV2':'12','MQD1':'22','MQF1':'20','QD1':'2','QD2':'6','QF2':'4','SNK1':'26'}
+    maparr = np.empty(len(eldict), dtype=object)
+    for i, names in enumerate(eldict.items()):
+        maparr[i] = (load_madx(names[0]), load_cosy(names[1]))
 
-for i, names in enumerate(eldict.items()):
-    print('')
-    print('{} (MADX) / {} (COSY)'.format(*names))
-    compare(maparr[i][0], maparr[i][1])
+    for i, names in enumerate(eldict.items()):
+        print('')
+        print('{} (MADX) / {} (COSY)'.format(*names))
+        compare(maparr[i][0], maparr[i][1])
 
-BH = load_madx('BH'); E50 = load_cosy('50')
-BV1 = load_madx('BV1'); E10 = load_cosy('10')
-BV2 = load_madx('BV2'); E12 = load_cosy('12')
-MQD1 = load_madx('MQD1'); E22 = load_cosy('22')
-MQF1 = load_madx('MQF1'); E20 = load_cosy('20')
-QD1 = load_madx('QD1'); E2 = load_cosy('2')
-QD1 = load_madx('QD2'); E6 = load_cosy('6')
-QF2 = load_madx('QF2'); E4 = load_cosy('4')
-SNK1 = load_madx('SNK1'); E26 = load_cosy('26')
+    BH = load_madx('BH'); E50 = load_cosy('50')
+    BV1 = load_madx('BV1'); E10 = load_cosy('10')
+    BV2 = load_madx('BV2'); E12 = load_cosy('12')
+    MQD1 = load_madx('MQD1'); E22 = load_cosy('22')
+    MQF1 = load_madx('MQF1'); E20 = load_cosy('20')
+    QD1 = load_madx('QD1'); E2 = load_cosy('2')
+    QD1 = load_madx('QD2'); E6 = load_cosy('6')
+    QF2 = load_madx('QF2'); E4 = load_cosy('4')
+    SNK1 = load_madx('SNK1'); E26 = load_cosy('26')
+
+    hz1 = 0.3739991
 
