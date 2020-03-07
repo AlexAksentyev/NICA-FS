4edae74356a64def5512c50b6e0d90b020f1a069
Alex Aksentyev <alexaksentyev@gmail.com>
2020-03-07 15:49:05 +0300

    analyzing decoherence

--- analysis/decoherence.py
+++ analysis/decoherence.py
@@ -3,105 +3,41 @@ import matplotlib.pyplot as plt; plt.ion()
 from importlib import reload
 import analysis as ana
 reload(ana)
-from scipy.optimize import curve_fit
 
-## for 3d animation
-from mpl_toolkits.mplot3d import Axes3D, proj3d
-from matplotlib import animation
+HOMEDIR, load_ps, load_sp, _read_header = ana.HOMEDIR, ana.load_ps, ana.load_sp, ana._read_header
 
-def get_arrow(index, S_X, S_Y, S_Z):
-    return 0, 0, 0, S_X[index], S_Y[index], S_Z[index]
+DATADIR = 'data/DECOH/'
 
-def update(index, S_X, S_Y, S_Z):
-    global quiver
-    quiver.remove()
-    quiver = ax0.quiver(*get_arrow(0, S_X, S_Y, S_Z), color='r')
-    quiver = ax0.quiver(*get_arrow(-1, S_X, S_Y, S_Z), color='k')
-    quiver = ax0.quiver(*get_arrow(index, S_X, S_Y, S_Z))
-##
+def plot_spin(dat):
+    fig, ax = plt.subplots(3,1,sharex=True)
+    ax[0].plot(dat['iteration'], dat['S_X']); ax[0].set_ylabel('S_X')
+    ax[1].plot(dat['iteration'], dat['S_Y']); ax[1].set_ylabel('S_Y')
+    ax[2].plot(dat['iteration'], dat['S_Z']); ax[2].set_ylabel('S_Z')
+    ax[2].set_xlabel('iteration')
 
-def plot_decoh_meas(data):
-    dm = {'X': data['S_X'].std(axis=1), 'Y': data['S_Y'].std(axis=1)}
-    fig, ax = plt.subplots(2,1, sharex=True)
-    ax[1].set_xlabel('turn')
-    for i, lbl in enumerate(['X','Y']):
-        ax[i].plot(data['turn'], dm[lbl])
-        ax[i].set_ylabel('RMS(S_{})'.format(lbl))
+def plot(dat):
+    fig, ax = plt.subplots(4,1,sharex=True)
+    ax[0].plot(dat['X']); ax[0].set_ylabel('X')
+    ax[1].plot(dat['Y']); ax[1].set_ylabel('Y')
+    ax[2].plot(dat['T']); ax[2].set_ylabel('T')
+    ax[3].plot(dat['D']); ax[3].set_ylabel('D')
 
-def load_data(path):
-    VARS = {
-        'ps': list(zip(['X','A','Y','B','T','D'], [float]*6)),
-        'sp': list(zip(['S_X','S_Y','S_Z'], [float]*3))
-        }
-    d_type = lambda case: list(zip(['turn', 'PID'], [int]*2)) + VARS[case]
-    
-    dat = {
-        'sp' : np.loadtxt(path + 'TRPSPI:TR.dat', d_type('sp'), skiprows=2),
-        'ps':  np.loadtxt(path + 'TRPRAY:TR.dat', d_type('ps'), skiprows=2)
-        }
-    nray = dat['sp']['PID'].max() + 1
-    for el in ['sp','ps']:
-        dat[el].shape = (-1, nray)
-        dat[el] = dat[el][:,1:]
-    nray -= 1
-    return dat, nray
-##
+def plot_ps(data, varx, vary, turns):
+    fig2, ax2 = plt.subplots(1,1)
+    ax2.plot(data[varx][:turns], data[vary][:turns], '-.')
+    ax2.set_ylabel(vary)
+    ax2.set_xlabel(varx)
+    ax2.ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
 
-HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
-DIR = 'data/DECOH/SHORT/INJECT/OPTIMAL/'
-dat, nray = load_data(HOME + DIR)
+def pol(dat):
+    PX, PY, PZ = (dat['S_'+e].sum(axis=1) for e in ['X','Y','Z'])
+    N = dat.shape[1]
+    return np.sqrt(PX**2 + PY**2 + PZ**2)/N
 
-bunch_num = 3
-a = ana.Analysis(dat['sp'][:, np.arange(bunch_num-1, nray, 4)])
-S_X, S_Y, S_Z = (a.S[lbl][np.arange(0, 1001, 50)] for lbl in ['X','Y','Z'])
-S = np.sqrt(S_X**2 + S_Y**2 + S_Z**2)
+if __name__ == '__main__':
+    ps = load_ps(HOMEDIR+DATADIR)
+    sp = load_sp(HOMEDIR+DATADIR)
+    P = pol(sp)
+    plt.plot(P)
+    plot(ps)
 
-fig0 = plt.figure()
-ax0 = fig0.add_subplot(111, projection='3d')
-ax0.set_xlabel('X')
-ax0.set_ylabel('Z')
-ax0.set_zlabel('Y')
-ax0.set_xlim(-1, 1)
-ax0.set_ylim(-1, 1)
-ax0.set_zlim(-1, 1)
-
-quiver = ax0.quiver(*get_arrow(0, S_X, S_Z, S_Y))
-
-anim = animation.FuncAnimation(fig0, update, fargs=(S_X, S_Z, S_Y), interval=10, blit=False, frames=S_X.shape[0])
-# anim.save('../reports/decoherence_10e6.gif')
-
-fig, ax = plt.subplots(1,1)
-nn = np.arange(0,a.Pval.shape[0])
-yy = a.Pval[nn]
-ftr = 1 # turns/point
-ax.plot(nn*ftr, yy, '--.')
-ax.ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
-ax.set_ylabel('P')
-ax.set_xlabel('turn #')
-popt = yy[1], (yy[-1]-yy[1])/(nn[-1]-nn[1])/ftr
-# popt, pcov = curve_fit(lambda x, a,b: a + b*x, nn, yy)
-ax.plot(nn*ftr, popt[0] + popt[1]*nn*ftr, 'r-', label=r'$(\beta_0 = {:4.2e}, \beta_1 = {:4.2e})$'.format(*popt))
-ax.legend()
-ax.set_title(DIR)
-
-def init():  # only required for blitting to give a clean slate.
-    line.set_ydata([np.nan] * X.shape[1])
-    return line,
-
-
-def animate(i):
-    line.set_data(X[i], A[i]) # update the data.
-    return line,
-
-elems = np.concatenate((np.arange(0,50,10), np.arange(100,400, 10), np.array([420, 450, 470, 471])))
-X, A = (dat['ps'][lbl] for lbl in ['T','D'])
-
-fig1, ax1 = plt.subplots()
-ax1.set_xlim(-2e-3, 2e-3)
-ax1.set_ylim(-2e-3, 2e-3)
-
-line, = ax1.plot(X[0], A[0], 'r.')
-
-
-ani = animation.FuncAnimation(fig1, animate, init_func=init,
-                                  interval=200, blit=True, save_count=50, frames=X.shape[0])
