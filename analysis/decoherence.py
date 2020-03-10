2a5a4e579e4860eb20c3c119665a2b20186bfa6d
Alex Aksentyev <alexaksentyev@gmail.com>
2020-03-10 14:10:38 +0300

    DECOHERENCE: computing the synchrotron oscillation frequency (TO BE FINISHED; right now, fitting doesn't work properly)

--- analysis/decoherence.py
+++ analysis/decoherence.py
@@ -3,10 +3,28 @@ import matplotlib.pyplot as plt; plt.ion()
 from importlib import reload
 import analysis as ana
 reload(ana)
+from scipy.optimize import curve_fit
 
-HOMEDIR, load_ps, load_sp, _read_header = ana.HOMEDIR, ana.load_ps, ana.load_sp, ana._read_header
+HOMEDIR, load_ps, load_sp = ana.HOMEDIR, ana.load_ps, ana.load_sp
 
 DATADIR = 'data/DECOH/'
+
+Fcyc = 286166.4863010005 # Hz
+
+def guess_freq(time, signal): # estimating the initial frequency guess
+    zci = np.where(np.diff(np.sign(signal)))[0] # find indexes of signal zeroes
+    delta_phase = np.pi*(len(zci)-1)
+    delta_t = time[zci][-1]-time[zci][0]
+    guess = delta_phase/delta_t/2/np.pi
+    return guess
+
+def guess_phase(time, sine):
+    ds = sine[1]-sine[0]
+    dt = time[1]-time[0]
+    sg = np.sign(ds/dt)
+    phase0 = np.arcsin(sine[0]) if sg>0 else np.pi-np.arcsin(sine[0])
+    return phase0
+
 
 def plot_spin(dat):
     fig, ax = plt.subplots(3,1,sharex=True)
@@ -24,7 +42,7 @@ def plot(dat):
 
 def plot_ps(data, varx, vary, turns):
     fig2, ax2 = plt.subplots(1,1)
-    ax2.plot(data[varx][:turns], data[vary][:turns], '-.')
+    ax2.plot(data[varx][:turns], data[vary][:turns], '.')
     ax2.set_ylabel(vary)
     ax2.set_xlabel(varx)
     ax2.ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
@@ -56,11 +74,11 @@ def plot_dm_angle(dat, same_axis=True, deg=True):
     else:
         fig, ax = plt.subplots(3,1,sharex=True)
         ax[0].plot(it, hd_meas, label='hor')
-        ax[0].set_ylabel(r'$\max(\theta_{xz})$' + ylabel_app)
+        ax[0].set_ylabel(r'$\sigma(\theta_{xz})$' + ylabel_app)
         ax[1].plot(it, vd_meas, label='vert')
-        ax[1].set_ylabel(r'$\max(\theta_{zy})$' + ylabel_app)
+        ax[1].set_ylabel(r'$\sigma(\theta_{zy})$' + ylabel_app)
         ax[2].plot(it, td_meas, label='tran')
-        ax[2].set_ylabel(r'$\max(\theta_{zy})$' + ylabel_app)
+        ax[2].set_ylabel(r'$\sigma(\theta_{zy})$' + ylabel_app)
         ax[2].set_xlabel('turn')
         for i in range(3):
             ax[i].ticklabel_format(style='sci', axis='y', scilimits=(0,0), useMathText=True)
@@ -92,7 +110,28 @@ def angles(s1, plane='H'):
     dp = s1[c1]*s0[c1] + s1[c2]*s0[c2]
     cos_phi = np.divide(dp/s0n, s1n, where=s1n!=0, out=np.zeros(s1n.shape))
     return np.arccos(cos_phi)
-    
+
+def synchrotron_osc(dat, plot=False):
+    fun = lambda x, a,b,p: a*np.sin(b*x + p)
+    nray = dat.shape[1]
+    stat = np.empty((nray, 3), dtype = list(zip(['EST','SE'], [float]*2)))
+    fig, ax = plt.subplots(1,1)
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0), useMathText=True)
+    ax.set_xlabel('turn')
+    ax.set_ylabel('D')
+    for i,ray in enumerate(dat.T):
+        it = ray['iteration']
+        d = ray['D']
+        a0 = d.max()
+        f0 = guess_freq(it, d/a0)
+        p0 = guess_phase(it, d/a0)
+        popt, pcov = curve_fit(fun, it, d, [a0, f0, p0])
+        perr = np.sqrt(np.diag(pcov))
+        stat[i]['EST'] = popt
+        stat[i]['SE']  = perr
+        ax.plot(it, d, '.')
+        ax.plot(it, fun(it, *popt), '--r')
+    return stat
 
 if __name__ == '__main__':
     ps0 = load_ps(HOMEDIR+DATADIR)
