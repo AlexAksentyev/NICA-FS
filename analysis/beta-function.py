53a04ba84ea080a92f4adfc503783c68a9471f83
Alex Aksentyev <alexaksentyev@gmail.com>
2021-08-27 22:00:44 +0300

    Updated test/beta-function,element_maps,test.fox for use with BENDS3 (instead of BENDS24); same with analysis/beta-function,test.py

--- analysis/beta-function.py
+++ analysis/beta-function.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 from analysis import HOMEDIR
 
-DATADIR = 'data/BETA-FUNCTION/BENDS24/SEQFULL/'
+DATADIR = 'data/BETA-FUNCTION/BENDS3/SEQFULL/'
 
 ELNAMES = np.load('nica_element_names.npy')
 ELNAMES = np.array([e+' ['+ str(i+1) + ']' for i,e in enumerate(ELNAMES)])
@@ -21,7 +21,7 @@ ax1[1].plot(beta['EL'], beta['2IM'], '--r', label=r'$\Im(\beta_y)$')
 ax1[1].set_ylabel(r'$\beta_y$')
 for i in range(2):
     ax1[i].legend()
-plt.xticks(ticks=beta['EL'], labels=ELNAMES[beta['EL']-1], rotation=60)
+#plt.xticks(ticks=beta['EL'], labels=ELNAMES[beta['EL']-1], rotation=60)
 
 fig, ax2 = plt.subplots(2,1,sharex=True)
 ax2[0].plot(mu['EL'], mu['1RE'], '-b',  label=r'$\Re(\mu_x)$')
@@ -32,4 +32,4 @@ ax2[1].plot(mu['EL'], mu['2IM'], '--r', label=r'$\Im(\mu_y)$')
 ax2[1].set_ylabel(r'$\mu_y$')
 for i in range(2):
     ax2[i].legend()
-plt.xticks(ticks=mu['EL'], labels=ELNAMES[mu['EL']-1], rotation=60)
+#plt.xticks(ticks=mu['EL'], labels=ELNAMES[mu['EL']-1], rotation=60)
--- analysis/test.py
+++ analysis/test.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
-from analysis import HOMEDIR, DAVEC, load_ps, load_sp
+from analysis import HOMEDIR, DAVEC, load_data
 
-DIR  = 'data/TEST/SPINTUNE/'
+DIR  = '../data/TEST/'
 
 def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     d_type = [('EL', int), ('PID', int)] + list(zip(['NU', 'NX','NY','NZ'], [float]*4))
@@ -12,4 +12,7 @@ def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     return dat[:, 1:]
 
 if __name__ == '__main__':
-    dat = load_tss()
+    dat = load_data(DIR, 'TRPRAY.dat')
+    fig, ax = plt.subplots(1,2)
+    ax[0].plot(dat['X'], dat['A'])
+    ax[1].plot(dat['Y'], dat['B'])
