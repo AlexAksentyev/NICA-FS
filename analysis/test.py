713798b7165ae350a546b68089a7cb130b472949
Alex Aksentyev <alexaksentyev@gmail.com>
2021-09-14 14:28:58 +0300

    WENT back to BENDS3. It seems to work (at least way better than FIRST-ST). Checked at 600 kTurns; there doesn't seem to be a non-symplecticity problem. Will check at 3 MTurns now.

--- analysis/test.py
+++ analysis/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 from analysis import HOMEDIR, DAVEC, load_data
 
-DIR  = '../data/TEST/FIRST-ST/'
+DIR  = '../data/TEST/BENDS3/SEQ/'
 
 def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     d_type = [('EL', int), ('PID', int)] + list(zip(['NU', 'NX','NY','NZ'], [float]*4))
@@ -11,11 +11,38 @@ def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     dat.shape = (-1, nray)
     return dat[:, 1:]
 
+def plot(dat, spdat, rng = slice(0,-1,50), pid = [1,2,3]):
+    fig, ax = plt.subplots(2,2)
+    ax[0,0].plot(dat[rng,pid]['X']*1000, dat[rng,pid]['A']*1000)
+    ax[0,0].set_xlabel('X [mm]'); ax[0,0].set_ylabel('A [mrad]')
+    ax[0,1].plot(dat[rng,pid]['Y']*1000, dat[rng,pid]['B']*1000)
+    ax[0,1].set_xlabel('Y [mm]'); ax[0,1].set_ylabel('B [mrad]')
+    ax[1,0].plot(dat[rng,pid]['T'], dat[rng,pid]['D'])
+    ax[1,0].set_xlabel('T'); ax[1,0].set_ylabel('D');
+    ax[1,0].ticklabel_format(style='sci', scilimits=(0,0), useMathText=True, axis='both')
+    ax[1,1].plot(spdat[rng, pid]['iteration']/1000, spdat[rng,pid]['S_Z'])
+    ax[1,1].set_xlabel('turn [x1000]'); ax[1,1].set_ylabel('S_Z')
+    #ax[1,1].ticklabel_format(style='sci', scilimits=(0,0), useMathText=True, axis='x')
+
+def plot_seq(dat, spdat, rng = slice(0,-1,50), pid = [1,2,3]):
+    ps1 = dat[dat[:,0]['iteration']<2]
+    sp1 = spdat[spdat[:,0]['iteration']<2]
+    eid = ps1['EID'][rng, pid]
+    fig, ax = plt.subplots(3,1)
+    ax[0].plot(eid, ps1[rng,pid]['X']*1000)
+    ax[0].set_xlabel('EID'); ax[0].set_ylabel('X [mm]')
+    ax[1].plot(eid, ps1[rng,pid]['Y']*1000)
+    ax[1].set_xlabel('EID'); ax[1].set_ylabel('Y[mm]')
+    ax[2].plot(eid, sp1[rng,pid]['S_Z'])
+    ax[2].set_xlabel('EID'); ax[2].set_ylabel('S_Z')
+    for i in range(3):
+        ax[i].grid()
+    
+
 if __name__ == '__main__':
     dat = load_data(DIR, 'TRPRAY.dat')
     spdat = load_data(DIR, 'TRPSPI.dat')
-    fig, ax = plt.subplots(2,2)
-    ax[0,0].plot(dat[:,1]['X'], dat[:,1]['A'])
-    ax[0,1].plot(dat[:,1]['Y'], dat[:,1]['B'])
-    ax[1,0].plot(dat[:,3]['X'], dat[:,3]['A'])
-    ax[1,1].plot(dat[:,3]['Y'], dat[:,3]['B'])
+    if DIR[-4:-1]=='SEQ':
+        plot_seq(dat, spdat)
+    else:
+        plot(dat, spdat)
