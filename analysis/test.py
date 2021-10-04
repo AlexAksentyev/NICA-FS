2e1c6ba5e7076f589a37c0911260cca978e0d419
Alex Aksentyev <alexaksentyev@gmail.com>
2021-10-04 10:27:41 +0300

    minor additions to test scripts

--- analysis/test.py
+++ analysis/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 from analysis import HOMEDIR, DAVEC, load_data
 
-DIR  = '../data/TEST/BENDS3/300000/'
+DIR  = '../data/TEST/FIRST-ST/3000000/NAVI-ON/'
 
 def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     d_type = [('EL', int), ('PID', int)] + list(zip(['NU', 'NX','NY','NZ'], [float]*4))
@@ -11,18 +11,19 @@ def load_tss(path=HOMEDIR+DIR+'MU.dat'):
     dat.shape = (-1, nray)
     return dat[:, 1:]
 
-def plot(dat, spdat, rng = slice(0,-1,50), pid = [1,2,3]):
+def plot(dat, spdat, rng = slice(0,-1,50), pid = [1,2,3], fmt='.-'):
     fig, ax = plt.subplots(2,2)
-    ax[0,0].plot(dat[rng,pid]['X']*1000, dat[rng,pid]['A']*1000)
+    ax[0,0].plot(dat[rng,pid]['X']*1000, dat[rng,pid]['A']*1000, fmt)
     ax[0,0].set_xlabel('X [mm]'); ax[0,0].set_ylabel('A [mrad]')
-    ax[0,1].plot(dat[rng,pid]['Y']*1000, dat[rng,pid]['B']*1000)
+    ax[0,1].plot(dat[rng,pid]['Y']*1000, dat[rng,pid]['B']*1000, fmt)
     ax[0,1].set_xlabel('Y [mm]'); ax[0,1].set_ylabel('B [mrad]')
-    ax[1,0].plot(dat[rng,pid]['T'], dat[rng,pid]['D'])
+    ax[1,0].plot(dat[rng,pid]['T'], dat[rng,pid]['D'], fmt)
     ax[1,0].set_xlabel('T'); ax[1,0].set_ylabel('D');
     ax[1,0].ticklabel_format(style='sci', scilimits=(0,0), useMathText=True, axis='both')
     ax[1,1].plot(spdat[rng, pid]['iteration']/1000, spdat[rng,pid]['S_Z'])
     ax[1,1].set_xlabel('turn [x1000]'); ax[1,1].set_ylabel('S_Z')
     #ax[1,1].ticklabel_format(style='sci', scilimits=(0,0), useMathText=True, axis='x')
+    return fig,ax
 
 def plot_seq(dat, spdat, pid = [1,2,3], itn=(1,)):
     if len(itn)==1:
@@ -46,12 +47,13 @@ def plot_seq(dat, spdat, pid = [1,2,3], itn=(1,)):
     ax[2].set_xlabel('EID'); ax[2].set_ylabel('S_Z')
     for i in range(3):
         ax[i].grid()
+    return fig, ax
     
 
 if __name__ == '__main__':
     dat = load_data(DIR, 'TRPRAY.dat')
     spdat = load_data(DIR, 'TRPSPI.dat')
     if DIR[-4:-1]=='SEQ':
-        plot_seq(dat, spdat)
+        fig, ax = plot_seq(dat, spdat)
     else:
-        plot(dat, spdat)
+        fig, ax = plot(dat, spdat)
