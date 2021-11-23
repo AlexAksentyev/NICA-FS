d39a52b86b398c6b6b51273fff876352cacbb403
Alex Aksentyev <alexaksentyev@gmail.com>
2021-11-23 21:39:33 +0300

    UPDATE: expanded for multiple-case testing

--- analysis/nbarvar2ST.py
+++ analysis/nbarvar2ST.py
@@ -4,14 +4,16 @@ from analysis import load_data, Polarization
 
 LATTICE = 'SECOND-ST'
 ENERGY = '130' # MeV
-NTURN = '80'
-DATDIR = '../data/'+LATTICE+'/'+ENERGY+'MeV/NAVI-VARI/'+NTURN+'/'
 
-def main(spin_psi='=PSInavi'):
-    dat = load_data(DATDIR, 'TRPRAY:PSI0spin{}.dat'.format(spin_psi))
-    spdat = load_data(DATDIR, 'TRPSPI:PSI0spin{}.dat'.format(spin_psi))
-    muarr = load_data(DATDIR, 'TRMUARR:PSI0spin{}.dat'.format(spin_psi))
+
+def main(case, spin_psi='=PSInavi'):
+    folder = '../data/'+LATTICE+'/'+ENERGY+'MeV/NAVI-VARI/RATE_'+case+'/'
+    dat = load_data(folder, 'TRPRAY:PSI0spin{}.dat'.format(spin_psi))
+    spdat = load_data(folder, 'TRPSPI:PSI0spin{}.dat'.format(spin_psi))
+    muarr = load_data(folder, 'TRMUARR:PSI0spin{}.dat'.format(spin_psi))
+    rate = np.diff(np.rad2deg(np.arcsin(muarr['NY'][:,0])))
+    print('psi <rate-of-change> = ', rate.mean(), '[deg/turn]')
     return dat, spdat, muarr
 
 if __name__ == '__main__':
-    dat, spdat, muarr = main()
+    dat, spdat, muarr = main('1e-2')
