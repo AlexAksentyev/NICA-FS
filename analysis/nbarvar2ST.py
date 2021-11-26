231442106b2e1d525a278844939a97fc71fd6331
Alex Aksentyev <alexaksentyev@gmail.com>
2021-11-26 16:21:03 +0300

    *** RESULTS ***: this multicase code version not only works, but the produced results indicate that with the increase of the psi rate-of-change spin-vector dispersion (a.k.a. decoherence) grows (but not the nbar-dispersion, which seems to be constant-value)

--- analysis/nbarvar2ST.py
+++ analysis/nbarvar2ST.py
@@ -29,15 +29,34 @@ def process(case_name):
     dat, spdat, muarr = load(case_name)
     disp = spin_disp(spdat)
     s0 = spdat[:,0] # reference spin vector
-    if True:
+    step = int(dat[1,0]['TURN'])
+    if True:                                                               # nbar plot
         fig1, ax1 = plt.subplots(3,1,sharex=True)
-        ax1[0].set_title(r'$\dot \psi = $ {} [deg/turn]'.format(case_name))
+        ax1[0].set_title(r'$\dot \psi = $ {} [deg/{}-turn]'.format(case_name,step))
         ax1[2].set_xlabel('TURN #')
         for i, var in enumerate(['X', 'Y', 'Z']):
-            ax1[i].plot(muarr['TURN'], muarr['N'+var], '--')
-            ax1[i].plot(s0['TURN'], s0['S_'+var], '-k')
-            ax1[i].set_ylabel(r'$\bar n_{}$ [$\vec s_0$]'.format(var))
-    return disp
+            ax1[i].plot(muarr['TURN'], muarr['N'+var], '-')
+            ax1[i].set_ylabel(r'$\bar n_{}$'.format(var))
+    if True:                                                        # spin-vector plot
+        fig3, ax3 = plt.subplots(3,1,sharex=True)
+        ax3[0].set_title(r'$\dot \psi = $ {} [deg/{}-turn]'.format(case_name,step))
+        ax3[2].set_xlabel('TURN #')
+        for i, var in enumerate(['X', 'Y', 'Z']):
+            ax3[i].plot(spdat[:,1:]['TURN'], spdat[:,1:]['S_'+var], '--')
+            ax3[i].plot(s0['TURN'], s0['S_'+var], '-k')
+            ax3[i].set_ylabel(r'$\vec s_{}$'.format(var))
+    if True:                                                        # phase space plot
+        fig2, ax2 = plt.subplots(3,1)
+        ax2[0].set_title(r'$\dot \psi = $ {} [deg/{:d}-turn]'.format(case_name,step))
+        ax2[0].plot(dat[:,:3]['X']*1e3, dat[:,:3]['A']*1e3)
+        ax2[0].set_xlabel('X [mm]'); ax2[0].set_ylabel('A [mrad]')
+        ax2[1].plot(dat[:,:3]['Y']*1e3, dat[:,:3]['B']*1e3)
+        ax2[1].set_xlabel('Y [mm]'); ax2[1].set_ylabel('B [mrad]')
+        ax2[2].plot(dat[:,:3]['T']*1e3, dat[:,:3]['D'])
+        ax2[2].set_xlabel('T [mm]'); ax2[2].set_ylabel('D [unit]')
+        for i in range(3):
+            ax2[i].ticklabel_format(style='sci',scilimits=(0,0),useMathText=True)
+    return disp, dat[:,0]['TURN']
 
 def main(case_names):
     disp_dict = {}
@@ -45,15 +64,15 @@ def main(case_names):
     ax.set_xlabel('TURN #')
     ax.set_ylabel(r'$\sigma[\cos(\vec s, \vec s_0)]$')
     for i, case in enumerate(case_names):
-        disp = process(case)
+        disp, nturn = process(case)
         disp_dict.update({case:disp})
-        ax.plot(disp, label=case)
+        ax.plot(nturn, disp, label=case)
     ax.legend(title=r'$\dot\psi_{navi}$ [deg/turn]')
     ax.ticklabel_format(style='sci',scilimits=(0,0),useMathText=True, axis='y')
     return disp_dict
 
 if __name__ == '__main__':
-    vals = ['1','2']#,'5']
-    pows = ['-2','-1']#,'0','1']
+    vals = ['5']
+    pows = ['-1', '0']
     case_names = np.array([[x+'e'+y for x in vals] for y in pows]).flatten()
     disp_dict = main(case_names)
