3c4188e638b73b6962911f7426333c8f4fda703e
Alex Aksentyev <alexaksentyev@gmail.com>
2022-02-02 18:15:39 +0300

    NRGVAR2ST: now I vary the beam kinetic energy by giving the gamma*G integer value to the MAIN procedure; the HEADER file is updated accordingly: a new SET_FOR_PROTONS procedure is added taking yG as input, PLUS in the NAVIGATOR procedure I check against different yG cases; if yG=0, then the snake solenoids should operate as spin-freezers additionally to their function as navigators, but at yG>1 the spin-freezing capability is turned off and the solenoids operate as only spin-navigators. ~~ NOTE ~~ since at INTEGER yG PHIY (spin rotation angle in an arc) is integer*PI, any NBAR orientation other that along z is IMPOSSIBLE. Check the diagram 2.3 in the respective document from YNF. PHIY determines the relattive orientations of the PHIspd and PHImpd in the given lattice; at integer yG these vectors are collinear, meaning they rotate the spin vector only along the z-axis -- you can't set navi_psi (angle between nbar and z-axis) other than 0 or 180 -- it's geometrically impossible

--- analysis/nrgvar2ST.py
+++ analysis/nrgvar2ST.py
@@ -8,43 +8,43 @@ PSVARS = list(zip(['X','A','Y','B','T','D'],[float]*6))
 
 LATTICE = 'SECOND-ST'
 NTURN = '30000'
-ROOT = '../data/'+LATTICE+'/MOMVARY/'
-datadir = lambda momentum: ROOT+str(momentum)+'MeV:c/'+NTURN
+ROOT = '../data/'+LATTICE+'/yGVARY/'
+datadir = lambda momentum: ROOT+str(momentum)+'/'+NTURN
 
-def load_tss(folder):
-    mu = DAVEC(folder+'MU:PSI0spin-0')
-    nbar = load_nbar(folder, '-0')
+def load_tss(folder, spin_psi=0):
+    mu = DAVEC(folder+'MU:PSI0spin-'+str(spin_psi))
+    nbar = load_nbar(folder, '-'+str(spin_psi))
     return mu, nbar
 
-def main(momentum):
-    dir_ = datadir(momentum)
+def main(varyvar, spin_psi=0):
+    dir_ = datadir(varyvar)
     folder  = dir_+'/NAVIPSI-0/'
     print(folder)
     navi_psi_rad = np.deg2rad(180-0) # 180 b/c the navigators set psi in the SPD as 180 - psi
     axis = [0, np.sin(navi_psi_rad), np.cos(navi_psi_rad)]
     print(axis)
     ## data loading
-    dat = load_data(folder, 'TRPRAY:PSI0spin-0.dat')
-    spdat = load_data(folder, 'TRPSPI:PSI0spin-0.dat')
+    dat = load_data(folder, 'TRPRAY:PSI0spin-{:d}.dat'.format(spin_psi))
+    spdat = load_data(folder, 'TRPSPI:PSI0spin-{:d}.dat'.format(spin_psi))
     P = Polarization.on_axis(spdat, axis)
     ## computations
-    mom = str(momentum)
+    vvl = str(varyvar) # "varyvar label"
     P.plot(1)
-    plt.savefig(folder+mom+'-pol.png', bbox_inches='tight', pad_inches=.1)
+    plt.savefig(folder+vvl+'-pol.png', bbox_inches='tight', pad_inches=.1)
     Px = Polarization.on_axis(spdat[1:-1:3], axis)
     Px.plot(1)
-    plt.savefig(folder+mom+'-pol-X-bunch.png', bbox_inches='tight', pad_inches=.1)
+    plt.savefig(folder+vvl+'-pol-X-bunch.png', bbox_inches='tight', pad_inches=.1)
     fig, ax = plot(dat, spdat)
-    plt.savefig(folder+mom+'-plots.png', bbox_inches='tight', pad_inches=.1)
+    plt.savefig(folder+vvl+'-plots.png', bbox_inches='tight', pad_inches=.1)
     fig2, ax2 = plot_spin(spdat)
-    plt.savefig(folder+mom+'-spin.png', bbox_inches='tight', pad_inches=.1)
+    plt.savefig(folder+vvl+'-spin.png', bbox_inches='tight', pad_inches=.1)
     plt.close('all')
     return P
 
-def spin_dyn(momentum):
-    dir_ = datadir(momentum)
+def spin_dyn(varyvar, spin_psi=0):
+    dir_ = datadir(varyvar)
     folder  = dir_+'/NAVIPSI-0/'
-    spdat = load_data(folder, 'TRPSPI:PSI0spin-0.dat')
+    spdat = load_data(folder, 'TRPSPI:PSI0spin-{:d}.dat'.format(spin_psi))
     # s0 = np.zeros(spdat.shape[0], dtype=list(zip(['X','Y','Z'],[float]*3)))
     # for lbl in ['X','Y','Z']:
     #     s0[lbl] = spdat[:,1]['S_'+lbl]
@@ -54,11 +54,11 @@ def _spana(P,pid1=11,pid2=14):
     fig, ax = plt.subplots(3,3,sharey='row',sharex='col')
     for i, lbl in enumerate(['X','Y','Z']):
         ax[i,0].set_ylabel('S_'+lbl)
-        for j, mom in enumerate([511,3200, 4800]):
+        for j, vvar in enumerate([0, 3, 4]):
             ax[i,j].plot(P[mom][:,pid1]['S_'+lbl],  label=str(pid1+1))
             ax[i,j].plot(P[mom][:,pid2]['S_'+lbl], label=str(pid2+1))
             ax[i,j].plot(P[mom][:,0]['S_'+lbl],  label='ref')
-            ax[0,j].set_title(str(mom)+' MeV/c')
+            ax[0,j].set_title('yG = '+str(vvar))
         ax[i,0].legend()
     return fig, ax
 
@@ -70,7 +70,7 @@ if __name__ == '__main__':
         caserng.remove('img');
     except:
         pass
-    caserng = [int(x[:-5]) for x in caserng]; caserng.sort()
+    caserng = [int(x) for x in caserng]; caserng.sort()
     P = {}
-    for mom in caserng:
-        P.update({mom: main(mom)})
+    for casevar in caserng:
+        P.update({casevar: main(casevar, 45)})
