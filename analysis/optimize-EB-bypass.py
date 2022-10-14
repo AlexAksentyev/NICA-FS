8a13bbf232b8c41b4091af6f0affc6f4ae7cff88
Alex Aksentyev <alexaksentyev@gmail.com>
2022-10-14 20:26:40 +0300

    BYPASS-SEX-CLEAR: EB optimization test and analysis rewrite

---
+++ analysis/optimize-EB-bypass.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt; plt.ion()
+from analysis import load_data
+
+
+DIR = '../data/BYPASS_SEX_CLEAR/'
+
+GAMMA = 1.1279235
+BETA = np.sqrt(GAMMA**2-1)/GAMMA # injection at y = 1.129 (242.01975 MeV) Deuterons
+CLIGHT = 3e8
+Lacc = 503.04
+v = CLIGHT*BETA
+TAU = Lacc/v
+
+def plot_spin(spdat, rng=slice(0,-1,50),pid = [1,2,3], title=''):
+    fig, ax = plt.subplots(3,1,sharex=True)
+    ax[0].set_title(title)
+    ax[0].plot(spdat[rng, pid]['iteration']/1000, spdat[rng,pid]['S_X'])
+    ax[0].set_ylabel('S_X')
+    ax[1].plot(spdat[rng, pid]['iteration']/1000, spdat[rng,pid]['S_Y'])
+    ax[1].set_ylabel('S_Y')
+    ax[2].plot(spdat[rng, pid]['iteration']/1000, spdat[rng,pid]['S_Z'])
+    ax[2].set_xlabel('turn [x1000]'); ax[2].set_ylabel('S_Z')
+    return fig, ax
+
+if __name__ == '__main__':
+    TAG = 'optimize-EB'
+
+    spdat_pre  = load_data(DIR, "TRPSPI:pre-{}.dat".format(TAG))
+    spdat_post = load_data(DIR, "TRPSPI:post-{}.dat".format(TAG))
+    plot_spin(spdat_pre); plot_spin(spdat_post)
