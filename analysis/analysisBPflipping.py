9a009f023386d30174df5e9053b5487d62085ed4
Alex Aksentyev <alexaksentyev@gmail.com>
2023-06-22 12:41:17 +0300

    added the 'reverse flip' test in which i turn on the RC to restore the n-bar to vertical orientation. since i'm standing in resonance, the RC is insufficient. this is as expected: when doing the actual experimentation we will also leave the resonance point. i'm not sure why we need the RC at all (except maybe in view of adiabaticity concerns).

--- analysis/analysisBPflipping.py
+++ analysis/analysisBPflipping.py
@@ -4,7 +4,8 @@ from analysis import DAVEC
 from glob import glob
 import re
 
-DIR = '../data/BYPASS_SEX_wRC/RESFLIP/'
+DIR = '../data/BYPASS_SEX_wRC/REVFLIP/'
+par_name = 'RCNU'
 mrkr_form = lambda n: 'CASE_{:d}'.format(n)
 case_sign = '*'
 
@@ -25,10 +26,12 @@ class NBAR:
 def load_data(dir):
     cases = [int(re.findall(r'\d+',e)[0]) for e in glob(DIR+'ABERRATIONS:'+case_sign)]
     cases.sort()
+    cases = np.arange(50)
     ncases = len(cases)
     nbar = {}; nu = {}
     n0 = np.zeros(ncases, dtype=list(zip(['X','Y','Z'],[float]*3))); nu0 = np.zeros(ncases)
-    EBe = np.zeros(ncases)
+    par = np.zeros(ncases)
+    pardict = {'EBE':2, 'RCNU':3}
     for i, case in enumerate(cases):
         print(case)
         nbar.update({case: NBAR(DIR, mrkr_form(case))})
@@ -36,8 +39,8 @@ def load_data(dir):
         tmp = [nbar[case].mean[e] for e in range(3)]
         n0[i] = tmp[0],tmp[1], tmp[2]
         nu0[i] = nu[case].const
-        EBe[i] = np.loadtxt(DIR+'/LATTICE-PARAMETERS:'+mrkr_form(case),skiprows=1)[2]
-    return nu, nbar, nu0, n0, EBe
+        par[i] = np.loadtxt(DIR+'/LATTICE-PARAMETERS:'+mrkr_form(case),skiprows=1)[pardict[par_name]]
+    return nu, nbar, nu0, n0, par
 
 def plot_nu_vs_EB(EBe, nu0):
     fig, ax = plt.subplots(1,1)
@@ -55,25 +58,26 @@ def plot_nbar_vs_EB(EBe, n0):
     ax[2].set_xlabel('EB E-field [kV/cm]')
     return fig, ax
 
-def plot_TSS_vs_EB(EBe, nu0, n0):
+def plot_TSS_vs_PAR(par, nu0, n0):
     fig, ax = plt.subplots(4,1,sharex=True)
+    xlabdict = {'RCNU':r'$\nu_{RC}$', 'EBE':'EB E-field [kV/cm]'}
     color = ['k','g','r','b']
-    ax[0].plot(EBe, nu0, color=color[0])
+    ax[0].plot(par, nu0, color=color[0])
     ax[0].set_ylabel(r'$\nu_0$')
     ax[0].ticklabel_format(style='sci', scilimits=(0,0), axis='both',useMathText=True)
     ax[0].grid()
     for i, crd in enumerate(['X','Y','Z']):
-        ax[i+1].plot(EBe, n0[crd], color=color[i+1])
+        ax[i+1].plot(par, n0[crd], color=color[i+1])
         ax[i+1].set_ylabel(r'$\bar n_{}$'.format(crd))
         ax[i+1].ticklabel_format(style='sci', scilimits=(0,0), axis='both',useMathText=True)
         ax[i+1].grid()
-    ax[3].set_xlabel('EB E-field [kV/cm]')
+    ax[3].set_xlabel(xlabdict[par_name])
     return fig, ax
 
 if __name__ == '__main__':
-    nu, nbar, nu0, n0, EBe = load_data(DIR)
+    nu, nbar, nu0, n0, par = load_data(DIR)
     tilts = np.loadtxt('../data/BYPASS_SEX_wRC/EB_GAUSS:RESFLIP.in')/np.pi * 180  # rad -> deg
     mean_tilt = tilts.mean()
     tilt_sd = tilts.std()
-    fig, ax = plot_TSS_vs_EB(EBe, nu0, n0)
+    fig, ax = plot_TSS_vs_PAR(par, nu0, n0)
     ax[0].set_title(r'tilts: {:4.2e} $\pm$ {:4.2e} [deg]'.format(mean_tilt, tilt_sd))
